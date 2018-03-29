b2fd607f66d8a3d33ce62cca2288faa33c53a46d
maxime.jenny <maxime.jenny@epitech.eu>
2018-03-29 13:18:28 +0200

    added a comment for the Core Class

--- core/Core.hpp
+++ core/Core.hpp
@@ -9,6 +9,10 @@
 #define CPP_ARCADE_CORE_HPP
 
 namespace Arcade {
+	// The core will be initialized by the main, then it will handle the program
+	// The purpose of the Core is to handle the main loop for the program
+	// Then load the actual lib chosen by the user and load the other libs when needed.
+	// Then Core will call the function to render the game and then the lib function to do when the game "asks"
 	class Core {
 	public:
 		Core();
